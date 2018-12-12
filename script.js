11ac0a601f8684451c534b1f3c380e894476a9d3
JasmineGarzon <44040686+JasmineGarzon@users.noreply.github.com>
2018-12-12 17:50:42 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".DownSmash1").click(function() {
+  $(".DownSmash").slideToggle();
+});
+    $(".UpSmash2").click(function() {
+  $(".UpSmash").slideToggle();
+});
+        $(".Special3").click(function() {
+  $(".Special").fadeToggle();
+});
